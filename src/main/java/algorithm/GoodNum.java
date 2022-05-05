ca656de4014178c461e283522011771f928a2246
KEUMIN <mskeum93@naver.com>
2022-05-05 21:14:54 +0900

    22/05/05 Good Number

---
+++ src/main/java/algorithm/GoodNum.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class GoodNum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int cnt = 0;
+        for (int i = 0; i < num; i++) {
+            int find = arr[i];
+            int start = 0;
+            int end = num - 1;
+
+            while (start < end) {
+                if (arr[start] + arr[end] == find) {
+                    if (start != i && end != i) {
+                        cnt++;
+                        break;
+                    } else if (start == i) {
+                        start++;
+                    } else if (end == i) {
+                        end--;
+                    }
+                } else if (arr[start] + arr[end] < find) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        System.out.println(cnt);
+        bf.close();
+    }
+}
