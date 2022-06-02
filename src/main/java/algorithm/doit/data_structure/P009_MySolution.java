2ad1712e4670388dbcd74575853bcc42807f0a0f
KEUMIN <mskeum93@naver.com>
2022-06-02 22:30:02 +0900

    22/06/02 알고리즘 연습 - 1회독 - 백준 12891번_슬라이딩윈도우_풀이

---
+++ src/main/java/algorithm/doit/data_structure/P009_MySolution.java
@@ -0,0 +1,33 @@
+package algorithm.doit.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class P009_MySolution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken()); // 목표 숫자
+        String str = br.readLine();
+        char[] A = new char[S];
+        for (int i = 0; i < S; i++) {
+            A[i] = str.charAt(i);
+        }
+        int[] D = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < D.length; i++) {
+            D[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = 0;
+        while (end != (S - 1) || start != (S - P)) {
+
+        }
+
+    }
+}
