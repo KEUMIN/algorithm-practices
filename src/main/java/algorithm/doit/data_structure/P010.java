6a5e39bd8ee95e40bfbce3829a9362e6a1ee5f02
KEUMIN <mskeum93@naver.com>
2022-06-07 21:40:25 +0900

    22/06/07 알고리즘 연습 - 1회독 - 백준 11003번_최솟값찾기_풀이

--- src/main/java/algorithm/doit/data_structure/P010.java
+++ src/main/java/algorithm/doit/data_structure/P010.java
@@ -1,8 +1,10 @@
 package algorithm.doit.data_structure;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -11,6 +13,7 @@ public class P010{
 	private static int[] A;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
@@ -18,8 +21,18 @@ public class P010{
 		st = new StringTokenizer(br.readLine());
 		Deque<Node> myDeque = new LinkedList<>();
 		for (int i = 0; i < N; i++) {
-			
+			int now = Integer.parseInt(st.nextToken());
+			while (!myDeque.isEmpty() && myDeque.getLast().value > now) {
+				myDeque.removeLast();
+			}
+			myDeque.addLast(new Node(now, i));
+			if (myDeque.getFirst().index <= i - L) {
+				myDeque.removeFirst();
+			}
+			bw.write(myDeque.getFirst().value + " ");
 		}
+		bw.flush();
+		bw.close();
 	}
 	static class Node {
 		public int value;
