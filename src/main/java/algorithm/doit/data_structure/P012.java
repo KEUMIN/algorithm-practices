0f8799de91eef797c5867b5a72b1f9a610626b73
KEUMIN <mskeum93@naver.com>
2022-06-10 22:57:53 +0900

    22/06/10 알고리즘 연습 - 1회독 - 백준 17298번_오큰수_구하기_문풀

--- src/main/java/algorithm/doit/data_structure/P012.java
+++ src/main/java/algorithm/doit/data_structure/P012.java
@@ -1,5 +1,19 @@
 package algorithm.doit.data_structure;
 
-public class P012 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
+public class P012 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] A = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+	}
 }
