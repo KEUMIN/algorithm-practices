71762683b6eb9fa53c6495cef64eb17495620f5a
KEUMIN <mskeum93@naver.com>
2022-06-11 23:02:30 +0900

    22/06/11 알고리즘 연습 - 1회독 - 백준 17298번_오큰수_구하기_문풀_완

--- src/main/java/algorithm/doit/data_structure/P012.java
+++ src/main/java/algorithm/doit/data_structure/P012.java
@@ -1,9 +1,12 @@
 package algorithm.doit.data_structure;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Iterator;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class P012 {
@@ -15,5 +18,25 @@ public class P012 {
 		for (int i = 1; i <= N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
+		int[] ans = new int[N];
+		Stack<Integer> myStack = new Stack<>();
+		myStack.push(0);
+		for (int i = 0; i < N; i++) {
+			while (!myStack.isEmpty() && A[myStack.peek()] < A[i]) {
+				ans[myStack.pop()] = A[i];
+				
+			}
+			myStack.push(i);
+		}
+				
+		while (!myStack.empty()) {
+			ans[myStack.pop()] = -1;
+		}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for (int i = 0; i < N; i++) {
+			bw.write(ans[i] + " ");
+		}
+		bw.write("\n");
+		bw.flush();
 	}
 }
