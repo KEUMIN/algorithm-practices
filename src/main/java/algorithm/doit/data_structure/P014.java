c9e97bfda23181d4de1ef463e662ce2ba9fb2203
KEUMIN <mskeum93@naver.com>
2022-06-13 22:31:07 +0900

    알고리즘 연습 - 1회독 - 백준 11268번_최솟값_힙_문풀

---
+++ src/main/java/algorithm/doit/data_structure/P014.java
@@ -0,0 +1,22 @@
+package algorithm.doit.data_structure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P014 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			if (tmp != 0) {
+				
+			} else {
+				
+			}
+		}
+	}
+}
