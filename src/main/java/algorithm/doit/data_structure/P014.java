7633d8be9b6eecf39c19db7a6146610c4315f20b
KEUMIN <mskeum93@naver.com>
2022-06-14 20:39:54 +0900

    알고리즘 연습 - 1회독 - 백준 11268번_최솟값_힙_문풀_완

--- src/main/java/algorithm/doit/data_structure/P014.java
+++ src/main/java/algorithm/doit/data_structure/P014.java
@@ -3,19 +3,29 @@ package algorithm.doit.data_structure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class P014 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] A = new int[N];
+		PriorityQueue<Integer> myQueue = new PriorityQueue<Integer>((o1, o2) -> {
+			int first_abs = Math.abs(o1);
+			int second_abs = Math.abs(o2);
+			if (first_abs == second_abs) return o1 > o2 ? 1 : -1;
+			else return first_abs - second_abs;
+		});
 		
 		for (int i = 0; i < N; i++) {
 			int tmp = Integer.parseInt(br.readLine());
-			if (tmp != 0) {
-				
+			if (tmp == 0) {
+				if (myQueue.isEmpty()) {
+					System.out.println("0");
+				} else {
+					System.out.println(myQueue.poll());
+				}
 			} else {
-				
+				myQueue.add(tmp);
 			}
 		}
 	}
