c0848386d47403444818149d37808f5db0f7c78b
KEUMIN <mskeum93@naver.com>
2022-07-08 22:24:23 +0900

    22/07/08 알고리즘 연습 - 1회독 - 백준 2023번_신기한 소수 찾기_완

--- src/main/java/algorithm/doit/data_structure/P024.java
+++ src/main/java/algorithm/doit/data_structure/P024.java
@@ -2,6 +2,8 @@ package algorithm.doit.data_structure;
 
 import java.util.Scanner;
 
+import static java.lang.Math.*;
+
 public class P024 {
     static int N;
     public static void main(String[] args) {
@@ -11,5 +13,29 @@ public class P024 {
         DFS(3, 1);
         DFS(5, 1);
         DFS(7, 1);
+    }
+
+    static void DFS(int a, int b) {
+        if (b == N) {
+            if (isPrime(a)) {
+                System.out.println(a);
+            }
+            return;
+        }
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if (isPrime(a * 10 + i)) {
+                DFS(a * 10 + i, b + 1);
+            }
+        }
+    }
+    static boolean isPrime(int x) {
+        if (x == 2 || x == 3) return true;
+        for (int i = 2; i <= sqrt(x); i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
     }
 }
